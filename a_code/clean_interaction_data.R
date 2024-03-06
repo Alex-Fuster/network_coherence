ca5532f8833a60439681fcbdf856660c3429fbbf
Gracielle Higino <graciellehigino@hotmail.com>
2024-03-05 16:34:24 -0800

    retrieve and clean bees interaction data

---
+++ a_code/clean_interaction_data.R
@@ -0,0 +1,48 @@
+# Packages loading --------------------------------------------------------
+
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+packages <- c("tidyverse", "googlesheets4")
+
+ipak(packages); rm(packages,ipak)
+
+# Load species data and splitting
+
+interaction_data_raw <- read_sheet("1FdADRdStEK-mYwXDi1bshKAHUib1S6Ftvfx_NXDeIbs", trim_ws = TRUE)
+
+interaction_data_clean <- interaction_data_raw |> 
+  select(c(id, genero_abelha_catalogo, especie_abelha_catalogo, subespecie_abelha_catalogo, genero_planta_catalogo, especie_planta_catalogo)) |> 
+  na.exclude() |> 
+  mutate(especie_abelha_catalogo = as.character(especie_abelha_catalogo))
+
+
+# Recoding values ---------------------------------------------------------
+interaction_data_clean$especie_abelha_catalogo <- interaction_data_clean$especie_abelha_catalogo |> 
+  recode("NULL" = "")
+
+interaction_data_clean$genero_planta_catalogo <- interaction_data_clean$genero_planta_catalogo |> 
+  recode("Eugenia (3 spp.)" = "Eugenia")
+
+interaction_data_clean$especie_planta_catalogo <- interaction_data_clean$especie_planta_catalogo |> 
+  recode("sp. (3 spp.)" = "spp.",
+         "sp. (3spp)" = "spp.",
+         "spp" = "spp.",
+         "2" = "spp.")
+
+# Binding species names columns -------------------------------------------
+interaction_data_clean <- 
+interaction_data_clean |> 
+  unite(col = "bee_species",
+        genero_abelha_catalogo:subespecie_abelha_catalogo,
+        sep = " ",
+        remove = TRUE) |> 
+  unite(col = "plat_species",
+        genero_planta_catalogo:especie_planta_catalogo,
+        sep = " ",
+        remove = TRUE)
+
