cd6934dad05430903a72e0251da26d934d0fe9ce
Gracielle Higino <graciellehigino@hotmail.com>
2024-03-08 17:30:28 -0800

    environmental data exploration

--- a_code/example_bees/03-Environmental_data.R
+++ a_code/example_bees/03-Environmental_data.R
@@ -29,35 +29,46 @@ environ_layers <- load_layers(sample(subset_env_layers, 1, replace = FALSE),
 
 # Define a boundary
 
-boundary = terra::ext(c(xmin = min(c(bees_occ_selected$decimalLongitude, plants_occ_selected$decimalLongitude)), 
-                        xmax = max(c(bees_occ_selected$decimalLongitude, plants_occ_selected$decimalLongitude)), 
-                        ymin = min(c(bees_occ_selected$decimalLatitude, plants_occ_selected$decimalLatitude)), 
-                        ymax = max(c(bees_occ_selected$decimalLatitude, plants_occ_selected$decimalLatitude))))
+boundary <- terra::ext(ma_shapefile)
 
 # Cropping environmental variables based on the extent of the occurrence data.
 # Notice the name of the variable changes with sampling, you might need to change
 # the value after the dollar sign for environ_layers.
 
-environ_layers_cropped <- terra::crop(environ_layers$WC_tmean7, boundary@ptr$vector)
+environ_layers_cropped <- terra::crop(environ_layers$WC_tmean1, boundary@cpp$vector)
 
 
 
 # Get environmental values by cell ----------------------------------------
 
-environ_values_bees <- terra::extract(environ_layers_cropped, select(bees_occ_selected, decimalLongitude, decimalLatitude))
+environ_values_pollinators <- terra::extract(environ_layers_cropped, select(pollinators_occ_selected, decimalLongitude, decimalLatitude))
 
 environ_values_plants <- terra::extract(environ_layers_cropped, select(plants_occ_selected, decimalLongitude, decimalLatitude))
 
 
 combined_df <- bind_rows(
-  bind_cols(species = bees_occ_selected$scientificName, environ_values = environ_values_bees),
-  bind_cols(species = plants_occ_selected$scientificName, environ_values = environ_values_plants)
+  bind_cols(species = pollinators_occ_selected$scientificName, 
+            environ_values = environ_values_pollinators,
+            group = "pollinators"),
+  bind_cols(species = plants_occ_selected$scientificName, 
+            environ_values = environ_values_plants,
+            group = "plants")
 )
 
   
 # Get environmental curve -------------------------------------------------
 
-ggplot(combined_df, aes(x=environ_values))+
+# Just plottin'
+ggplot(combined_df, aes(x=environ_values, color=group)) +
   geom_density()
 
-
+# Explorin'
+combined_df |> 
+  group_by(group) |> 
+  drop_na() |> 
+  summarise(
+    min = min(environ_values),
+    max = max(environ_values),
+    mean = mean(environ_values),
+    sd = sd(environ_values)
+    )
