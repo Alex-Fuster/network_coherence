c1a776d5a3a7c634047742e06d5b4d68b0413463
Gracielle Higino <graciellehigino@hotmail.com>
2024-04-22 19:26:43 -0700

    :zap: simplify data cleaning

--- a_code/example_bees/04-clean_data.R
+++ a_code/example_bees/04-clean_data.R
@@ -2,10 +2,6 @@
 # Load packages -----------------------------------------------------------
 
 library(tidyverse)
-library(bdc)
-library(taxadb)
-library(rgbif)
-library(dismo)
 
 # Load data ----------------------------------------------------------------
 
@@ -13,21 +9,17 @@ load("b_data/gbif_occ_selected.Rdata")
 
 # Sampling bias ------------------------------------------------------------
 
-# sample:
-pollinators_occ_unique <- gridSample(data.frame(pollinators_occ_selected[,c("decimalLongitude","decimalLatitude")]),
-                   environ_layers_cropped, n=1)
-
-plants_occ_unique <- gridSample(data.frame(plants_occ_selected[,c("decimalLongitude","decimalLatitude")]),
-                                     environ_layers_cropped, n=1)
-
-# Merge datasets
-
-plants_occ_selected <- plants_occ_selected |> 
-  semi_join(plants_occ_unique, by = c("decimalLatitude", "decimalLongitude"))
-
-
+# Sample unique points for each species
 pollinators_occ_selected <- pollinators_occ_selected |>
-  semi_join(pollinators_occ_unique, by = c("decimalLatitude", "decimalLongitude"))
+  group_by(species) |> 
+  distinct(decimalLatitude, decimalLongitude) |> 
+  ungroup()
+
+
+plants_occ_selected <- plants_occ_selected |>
+  group_by(species) |> 
+  distinct(decimalLatitude, decimalLongitude) |> 
+  ungroup()
 
 # Exclude species with only one point
 
