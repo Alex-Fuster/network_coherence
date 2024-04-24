ccf7828e4fdc1c885fd4db0c3cdca9e401fe1788
tutudocerrado <tutudocerrado@gmail.com>
2024-04-24 20:16:12 -0300

    add cell values

--- a_code/example_bees/05-curves.R
+++ a_code/example_bees/05-curves.R
@@ -1,17 +1,27 @@
 # Get environmental values by cell ----------------------------------------
 
-environ_values_pollinators <- terra::extract(environ_layers_cropped, dplyr::select(pollinators_occ_selected, decimalLongitude, decimalLatitude))
+environ_values_pollinators <- terra::extract(environ_layers_cropped, 
+                                             dplyr::select(pollinators_occ_selected, 
+                                                           decimalLongitude, 
+                                                           decimalLatitude),
+                                             cells = TRUE)
 
-environ_values_plants <- terra::extract(environ_layers_cropped, dplyr::select(plants_occ_selected, decimalLongitude, decimalLatitude))
+environ_values_plants <- terra::extract(environ_layers_cropped, 
+                                        dplyr::select(plants_occ_selected, 
+                                                      decimalLongitude, 
+                                                      decimalLatitude),
+                                        cells = TRUE)
 
 
 combined_df <- bind_rows(
   bind_cols(species = pollinators_occ_selected$species, 
-            environ_values = environ_values_pollinators,
-            group = "pollinators"),
+            environ_values = environ_values_pollinators$WC_tmean1,
+            group = "pollinators",
+            cell = environ_values_pollinators$cell),
   bind_cols(species = plants_occ_selected$species, 
-            environ_values = environ_values_plants,
-            group = "plants")
+            environ_values = environ_values_plants$WC_tmean1,
+            group = "plants",
+            cell = environ_values_plants$cell)
 )
 
 
