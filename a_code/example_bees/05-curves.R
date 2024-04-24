cf6646848ed7e124017052489bac694ece1b4286
Gracielle Higino <graciellehigino@hotmail.com>
2024-04-24 16:48:58 -0700

    add coordinates to dataset

--- a_code/example_bees/05-curves.R
+++ a_code/example_bees/05-curves.R
@@ -4,24 +4,30 @@ environ_values_pollinators <- terra::extract(environ_layers_cropped,
                                              dplyr::select(pollinators_occ_selected, 
                                                            decimalLongitude, 
                                                            decimalLatitude),
-                                             cells = TRUE)
+                                             cells = TRUE,
+                                             xy = TRUE)
 
 environ_values_plants <- terra::extract(environ_layers_cropped, 
                                         dplyr::select(plants_occ_selected, 
                                                       decimalLongitude, 
                                                       decimalLatitude),
-                                        cells = TRUE)
+                                        cells = TRUE,
+                                        xy = TRUE)
 
 
 combined_df <- bind_rows(
   bind_cols(species = pollinators_occ_selected$species, 
             environ_values = environ_values_pollinators$WC_tmean1,
             group = "pollinators",
-            cell = environ_values_pollinators$cell),
+            cell = environ_values_pollinators$cell,
+            longitude = environ_values_pollinators$x,
+            latitude = environ_values_pollinators$y),
   bind_cols(species = plants_occ_selected$species, 
             environ_values = environ_values_plants$WC_tmean1,
             group = "plants",
-            cell = environ_values_plants$cell)
+            cell = environ_values_plants$cell,
+            longitude = environ_values_plants$x,
+            latitude = environ_values_plants$y)
 )
 
 
