3cdcfaf92caf48ff0c35f150822b848e202e8974
tutudocerrado <tutudocerrado@gmail.com>
2024-04-24 22:52:49 -0300

    clean interaction matrix

--- a_code/example_bees/04-clean_data.R
+++ a_code/example_bees/04-clean_data.R
@@ -7,30 +7,52 @@ library(tidyverse)
 
 load("b_data/gbif_occ_selected.Rdata")
 
+# Combine datasets --------------------------------------------------------
+
+combined_df <- bind_rows(
+  bind_cols(species = pollinators_occ_selected$species, 
+            environ_values = environ_values_pollinators$WC_tmean1,
+            group = "pollinators",
+            cell = environ_values_pollinators$cell,
+            longitude = environ_values_pollinators$x,
+            latitude = environ_values_pollinators$y),
+  bind_cols(species = plants_occ_selected$species, 
+            environ_values = environ_values_plants$WC_tmean1,
+            group = "plants",
+            cell = environ_values_plants$cell,
+            longitude = environ_values_plants$x,
+            latitude = environ_values_plants$y)
+)
+
 # Sampling bias ------------------------------------------------------------
 
 # Sample unique points for each species
-pollinators_occ_selected <- pollinators_occ_selected |>
+
+combined_df <- combined_df |>
   group_by(species) |> 
-  distinct(decimalLatitude, decimalLongitude) |> 
+  distinct(cell, species, .keep_all = TRUE) |> 
   ungroup()
 
-
-plants_occ_selected <- plants_occ_selected |>
-  group_by(species) |> 
-  distinct(decimalLatitude, decimalLongitude) |> 
-  ungroup()
 
 # Exclude species with only one point
 
-plants_occ_selected <- plants_occ_selected |> 
+combined_df <- combined_df |> 
   group_by(species) |> 
   filter(n() > 1,
          species != "") |> 
   ungroup()
 
-pollinators_occ_selected <- pollinators_occ_selected |>
-  group_by(species) |>
-  filter(n() > 1,
-         species != "")|> 
-  ungroup()
+# Clean interaction matrix ------------------------------------------------
+
+interaction_matrix <- interaction_matrix |>
+  filter(spp_plants %in% plants_occ_selected$species,
+         spp_pollinators %in% pollinators_occ_selected$species)
+
+
+
+
+
+
+
+
+
