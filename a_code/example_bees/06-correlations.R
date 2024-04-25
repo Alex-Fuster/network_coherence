6b82b8924e864646a814744d88eb258753494022
Gracielle Higino <graciellehigino@hotmail.com>
2024-04-24 20:55:25 -0700

    correlation values for interacting and non-interacting spp

--- a_code/example_bees/06-correlations.R
+++ a_code/example_bees/06-correlations.R
@@ -1,20 +1,27 @@
 source("a_code/example_bees/01-get_interaction_data.R")
+source("a_code/example_bees/01.1-get_biome_shapefile.R")
+source("a_code/example_bees/03-Environmental_data.R")
+source("a_code/example_bees/04-clean_data.R")
+source("a_code/example_bees/05-derivatives.R")
 
 
 interaction_matrix <- interaction_matrix |>
   mutate(correlation = NA)
 
 
+# Create loop to calculate the correlation between interacting species --------
+
 for (i in 1:nrow(interaction_matrix)) {
-  ifelse(interaction_matrix$weighted_interaction > 0,
-    
+  # if(interaction_matrix$weighted_interaction[i] > 0){
     plant <- filter(combined_df, species == interaction_matrix$spp_plants[i])
     pollinator <- filter(combined_df, species == interaction_matrix$spp_pollinators[i])
     cor_df <- merge(plant, pollinator, by = "cell", suffixes = c("_plant","_pollinator"))
     interaction_matrix$correlation[i] <- cor(cor_df$derivative_plant, 
                                              cor_df$derivative_pollinator, 
-                                             method = "spearman"),
-
-    interaction_matrix$correlation[i] <- NA
+                                             method = "spearman")
+  # } else { 
+  #   interaction_matrix$correlation[i] <- NA
+  # }
 }
+
 
